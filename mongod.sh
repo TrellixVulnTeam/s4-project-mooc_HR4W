7902d7c039d9ddca61d4c4368b19a09277dd1d9d
yentledo <yentl.geldmeyer@student.howest.be>
2015-06-15 11:00:05 +0200

    mongofixfileInS4

---
+++ mongod.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo rm /edx/var/mongo/mongodb/mongod.lock
+sudo mongod -repair --config /etc/mongod.conf
+sudo chown -R mongodb:mongodb /edx/var/mongo/mongodb/.
+sudo service mongodb start
