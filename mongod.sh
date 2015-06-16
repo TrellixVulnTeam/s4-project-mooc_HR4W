e08d41d622972a0872564b0a5dc9f40e91b17b4a
Anton ve <anton.van.eechaute@student.howest.be>
2015-06-16 13:59:12 +0200

    fix mongo reset

--- mongod.sh
+++ mongod.sh
@@ -2,4 +2,4 @@
 sudo rm /edx/var/mongo/mongodb/mongod.lock
 sudo mongod -repair --config /etc/mongod.conf
 sudo chown -R mongodb:mongodb /edx/var/mongo/mongodb/.
-sudo service mongodb start
+sudo service mongod start
